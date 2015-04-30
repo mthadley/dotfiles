da09252e903ff3f1ea9ab0885a0edfd57e7c5db6
Michael Hadley <michael.hadley@liferay.com>
2015-04-30 09:55:42 -0700

    Remove unused scripts

--- Scripts/transmission-check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-# Transmission stats
-
-ACTIVE=$(expr $(transmission-remote -tactive -l | wc -l) - 2)
-TOTAL=$(expr $(transmission-remote -l | wc -l) - 2)
-
-echo -n "$ACTIVE/$TOTAL"
