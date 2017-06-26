5c7122c156f9db0bee83fb5d1c730743f3c863e2
Michael Hadley <mikethadley@gmail.com>
2017-06-25 22:42:04 -0700

    Add fish configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,21 @@
+# config.fish
+# Author: Michael T. Hadley (mikethadley@gmail.com)
+
+fish_vi_key_bindings
+
+set -x EDITOR vim
+set -x fish_greeting ''
+set -x PATH $PATH "$HOME/.cargo/bin"
+
+# aliases
+
+alias aa 'ant all'
+alias gsh 'telnet localhost 11311'
+alias npr 'npm run'
+alias tm 'tmux -2'
+
+## git
+alias gd 'git diff'
+alias gst 'git status'
+alias gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
+alias gplog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit --date=relative"
---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,37 @@
+function fish_prompt
+  if not set -q -g __mthadley_fish_prompt_defined
+    set -g __mthadley_fish_prompt_defined
+    function _git_branch_name
+      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    end
+
+    function _is_git_dirty
+      echo (git status -s --ignore-submodules=dirty ^/dev/null)
+    end
+
+    function _is_git_repo
+      type -q git; or return 1
+      git status -s >/dev/null ^/dev/null
+    end
+  end
+
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l normal (set_color normal)
+
+  set -l cwd $cyan(basename (prompt_pwd))
+
+  if _is_git_repo
+    set -l repo_branch $red(_git_branch_name)
+    set repo_info "$blue ⤷ $repo_branch$blue"
+
+    if test (_is_git_dirty)
+      set -l dirty "$yellow *"
+      set repo_info "$repo_info$dirty"
+    end
+  end
+
+  echo -n -s $cwd $repo_info $normal ' '
+end
