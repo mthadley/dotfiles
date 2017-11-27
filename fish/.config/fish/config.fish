5087906e1a4bc04589d6d082fb4158f7d50602c0
Michael Hadley <michael.hadley@liferay.com>
2017-11-27 15:25:49 -0800

    Update fish and tmux configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,7 @@ bind -M insert -m default jk backward-char force-repaint
 set -x EDITOR vim
 set -x fish_greeting ''
 set -x PATH $PATH "$HOME/.cargo/bin"
+set -x ANT_OPTS '-Xmx2048m'
 
 # aliases
 
@@ -21,3 +22,6 @@ alias gd 'git diff'
 alias gst 'git status'
 alias gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
 alias gplog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset' --abbrev-commit --date=relative"
+
+## java version manager
+[ -s "/Users/mike/.jabba/jabba.fish" ]; and source "/Users/mike/.jabba/jabba.fish"
