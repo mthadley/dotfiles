5285207dc854c8282ae74df1ccb5d7ea1ba1b516
Michael Hadley <mikethadley@gmail.com>
2019-07-06 10:15:23 -0700

    Make gb more robust and useful

--- fish/.config/fish/functions/gb.fish
+++ fish/.config/fish/functions/gb.fish
@@ -1,7 +1,13 @@
-function gb --description 'Check out a git branch'
-  set -l branch (git branch | fzf --header="gb - select a branch" | string trim)
+function gb --description "Check out a git branch"
+  set selection (
+    git for-each-ref --format="%(refname:short)" --sort="-committerdate" "refs/heads" |\
+    fzf \
+      --header="gb - select a branch" \
+      --preview="git show --no-patch --pretty=format:'%an - %s' {}" \
+      --preview-window down:1
+  )
 
-  if [ -n $branch ]
-    git checkout $branch
+  if test $status -eq 0
+    git checkout $selection
   end
 end
