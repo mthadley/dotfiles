b523d2dfb8c6d585417433137655a16d99b70630
Michael Hadley <mikethadley@gmail.com>
2019-11-20 17:59:29 -0800

    Use fetchFromGitHub instead

--- overlays/base16.nix
+++ overlays/base16.nix
@@ -4,9 +4,11 @@ self: super:
   base16 = self.stdenv.mkDerivation {
     name = "base16";
 
-    src = fetchGit {
-      url = "git@github.com:chriskempson/base16-shell.git";
-      ref = "master";
+    src = super.fetchFromGitHub {
+      owner = "chriskempson";
+      repo = "base16-shell";
+      rev = "ce8e1e540367ea83cc3e01eec7b2a11783b3f9e1";
+      sha256 = "1yj36k64zz65lxh28bb5rb5skwlinixxz6qwkwaf845ajvm45j1q";
     };
 
     dontBuild = true;
