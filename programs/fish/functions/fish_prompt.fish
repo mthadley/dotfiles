4466db4fae504b124b3fd2a2cde1149975bda96b
Michael Hadley <mikethadley@gmail.com>
2020-05-08 17:43:01 -0700

    Show last status in prompt

--- programs/fish/functions/fish_prompt.fish
+++ programs/fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt
+  set last_status $status
+
   if not set -q -g __mthadley_fish_prompt_defined
     set -g __mthadley_fish_prompt_defined
     function _git_branch_name
@@ -15,17 +17,23 @@ function fish_prompt
     end
   end
 
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l normal (set_color normal)
+  set yellow (set_color -o yellow)
+  set red (set_color -o red)
+  set blue (set_color -o blue)
+  set green (set_color -o green)
+  set normal (set_color normal)
 
-  set -l cwd $cyan (basename (prompt_pwd))
+  if test $last_status -eq 0
+    set status_color $green
+  else
+    set status_color $red
+  end
+
+  set cwd $status_color (basename (prompt_pwd))
 
   if _is_git_repo
-    set -l repo_branch $red(_git_branch_name)
-    set repo_info "$blue -> $repo_branch$blue"
+    set -l repo_branch $blue(_git_branch_name)
+    set repo_info "$blue » $repo_branch"
 
     if test (_is_git_dirty)
       set -l dirty "$yellow *"
