5b6b46453f3852cdf13307d2b2769fc8617b9fcf
Michael Hadley <mikethadley@gmail.com>
2020-05-08 17:57:42 -0700

    Keep the colors, remove last status

--- programs/fish/functions/fish_prompt.fish
+++ programs/fish/functions/fish_prompt.fish
@@ -18,18 +18,11 @@ function fish_prompt
   end
 
   set yellow (set_color -o yellow)
-  set red (set_color -o red)
   set blue (set_color -o blue)
   set green (set_color -o green)
   set normal (set_color normal)
 
-  if test $last_status -eq 0
-    set status_color $green
-  else
-    set status_color $red
-  end
-
-  set cwd $status_color (basename (prompt_pwd))
+  set cwd $green (basename (prompt_pwd))
 
   if _is_git_repo
     set -l repo_branch $blue(_git_branch_name)
