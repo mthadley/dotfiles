e25c0095d9176c6f82cc43053c599a18080f7bee
Michael Hadley <mikethadley@gmail.com>
2020-05-23 12:26:16 -0700

    Explicitly set fish color theme

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -58,6 +58,31 @@
 
       # Color scheme
       eval sh ${pkgs.base16}/share/base16/scripts/base16-ocean.sh
+
+      # Copied from the pre 3.0.0 default fish theme
+      set fish_color_autosuggestion 555 brblack
+      set fish_color_cancel -r
+      set fish_color_command --bold
+      set fish_color_comment red
+      set fish_color_cwd green
+      set fish_color_cwd_root red
+      set fish_color_end brmagenta
+      set fish_color_error brred
+      set fish_color_escape bryellow --bold
+      set fish_color_history_current --bold
+      set fish_color_host normal
+      set fish_color_host_remote yellow
+      set fish_color_match --background=brblue
+      set fish_color_normal normal
+      set fish_color_operator bryellow
+      set fish_color_param cyan
+      set fish_color_quote yellow
+      set fish_color_redirection brblue
+      set fish_color_search_match bryellow  --background=brblack
+      set fish_color_selection white --bold --background=brblack
+      set fish_color_status red
+      set fish_color_user brgreen
+      set fish_color_valid_path --underline
     '';
   };
 }
