7c1e513fc0b44481382d513014572b8ee2535ee6
Michael Hadley <mikethadley@gmail.com>
2020-06-18 21:17:11 -0700

    Organize some things

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -20,18 +20,20 @@
     secureSocket = !pkgs.stdenv.isDarwin;
 
     extraConfig = ''
+      # general options
+      set -g mouse on
+
+      # status line
       set -g status-right ""
       set -g status-justify centre
-
-      # Fixes colors in vim
-      set-option -sa terminal-overrides ',xterm-256color:RGB'
-
-      set -g mouse on
 
       # colors
       set-option -g status-style fg="#7f8894",bg="#343d46"
       set-option -g pane-border-style fg="#343d46"
       set-window-option -g window-status-current-style fg=green
+
+      # Fixes colors in vim
+      set-option -sa terminal-overrides ',xterm-256color:RGB'
 
       bind-key v split-window -h
       bind-key s split-window -v
