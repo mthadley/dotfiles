d3fa5288b5fcaf06e0c5b7e1eedcff43722afc79
Michael Hadley <mikethadley@gmail.com>
2020-09-12 10:40:22 -0700

    Rewrite similar-sort in Rust
    The download size was huge every time I updated my nix-channels and
    rebuilt the home environment partly because of GHC. Rust is at least
    half of that size, and the performance feels much faster to boot.

--- pkgs/similar-sort/default.nix
+++ pkgs/similar-sort/default.nix
@@ -1,20 +1,17 @@
-{ stdenv, haskellPackages, ...}:
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation {
-  name = "similar-sort";
-
-  buildInputs = [
-    (haskellPackages.ghcWithPackages (haskellPkgs: with haskellPkgs; [ edit-distance-linear ]))
-  ];
-
-  buildPhase = ''
-    ghc -O2 -Wall -Werror similar-sort.hs
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp similar-sort $out/bin
-  '';
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "similar-sort";
+  version = "1.0.0";
 
   src = ./.;
+
+  cargoSha256 = "113di4cgxl60689rmii9q36s3iz4w9gnr0j901bqqspgq2wfzxj6";
+
+  verifyCargoDeps = true;
+
+  meta = with pkgs.stdenv.lib; {
+    description = "Sorts stdin by how similar to the given string";
+    platforms = platforms.all;
+  };
 }
---
+++ pkgs/similar-sort/src/main.rs
@@ -0,0 +1,18 @@
+use strsim::{levenshtein};
+use std::{env, io::{self, BufRead}};
+
+fn main() {
+    let compare_to = env::args().nth(1).expect("Expected string to compare against.");
+
+    let mut lines = io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.expect("Failed to read line from stdin."))
+        .collect::<Vec<String>>();
+
+    lines.sort_by_cached_key(|line| levenshtein(line, &compare_to));
+
+    for line in lines {
+        println!("{}", line);
+    }
+}
