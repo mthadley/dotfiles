43f828500dbf50108dcae06fb54e63a6ba35f6cf
Michael Hadley <mikethadley@gmail.com>
2021-08-07 09:43:18 -0700

    Fix italics inside of vim + tmux

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -13,7 +13,7 @@
     historyLimit = 10000;
     keyMode = "vi";
     shortcut = "a";
-    terminal = "screen-256color";
+    terminal = "tmux-256color";
     escapeTime = 10;
 
     # This tries to set $TMUX_TMPDIR to /run/... which doesn't
