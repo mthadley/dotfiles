b87a6642acb6bd8a0c487d5f7c575e5162d1947a
Michael Hadley <mikethadley@gmail.com>
2022-04-23 16:53:51 -0700

    Cleanup `lspconfig`

--- programs/neovim/lua/mthadley/lspconfig.lua
+++ programs/neovim/lua/mthadley/lspconfig.lua
@@ -1,8 +1,8 @@
-local opts = { noremap=true, silent=true }
+local key_opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', key_opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', key_opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', key_opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -10,18 +10,23 @@ local function on_attach(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  local mappings = {
+    { 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>' },
+    { 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>' },
+    { 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>' },
+    { 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>' },
+    { 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>' },
+    { 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>' },
+    { 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>' },
+    { 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>' },
+    { 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>' },
+    { 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>' },
+  }
+
+  for _, key_mapping in pairs(mappings) do
+    local mode, keys, cmd = unpack(key_mapping)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, keys, cmd, key_opts)
+  end
 end
 
 local language_servers = { 'tsserver' }
