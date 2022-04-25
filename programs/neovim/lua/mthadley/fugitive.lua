1a00482295475c5cc345e3d20058b34e88fec931
Michael Hadley <mikethadley@gmail.com>
2022-04-24 22:53:39 -0700

    Fix typo in group var

--- programs/neovim/lua/mthadley/fugitive.lua
+++ programs/neovim/lua/mthadley/fugitive.lua
@@ -9,11 +9,11 @@ for _, mapping in ipairs({
   vim.keymap.set("n", keys, cmd)
 end
 
-local augroup = vim.api.nvim_create_augroup('fugitive', { clear = true })
+local group = vim.api.nvim_create_augroup('fugitive', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
   desc = 'Check spelling for Git commits.',
   pattern = 'gitcommit',
-  group = auogroup,
+  group = group,
   callback = function(opt) vim.opt_local.spell = true end,
 })
