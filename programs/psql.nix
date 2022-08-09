f8f646a6ad43ac32a830084b2a9f16ae3c095683
Michael Hadley <mikethadley@gmail.com>
2022-08-08 17:08:26 -0700

    Use home file shorthand

--- programs/psql.nix
+++ programs/psql.nix
@@ -1,13 +1,8 @@
 {
-  home = {
-    file.psqlrc = {
-      text = ''
-        \x auto
-        \pset linestyle unicode
-        \set PROMPT1 '%n@%m/%/ %R> '
-        \set PROMPT2 '%w'
-      '';
-      target = ".psqlrc";
-    };
-  };
+  home.file.".psqlrc".text = ''
+    \x auto
+    \pset linestyle unicode
+    \set PROMPT1 '%n@%m/%/ %R> '
+    \set PROMPT2 '%w'
+  '';
 }
