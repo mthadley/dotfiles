a61aabeeb1410478f2b97f9d7a69801f0c0ea2eb
Michael Hadley <m@mthadley.com>
2022-10-11 09:43:57 -0700

    Use ripgrep for file listing command in shell

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }:
 
+let
+  defaultFilesCommand = "${pkgs.ripgrep}/bin/rg --files";
+in
 {
   programs.fzf = {
     enable = true;
 
-    defaultCommand = "${pkgs.ripgrep}/bin/rg --files";
+    defaultCommand = defaultFilesCommand;
+    fileWidgetCommand = defaultFilesCommand;
+
     defaultOptions = [ "--color=16" ];
 
     enableFishIntegration = true;
