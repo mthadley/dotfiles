8d128bd2ba4ecd4ad1380c3756470b60dd700940
Michael Hadley <m@mthadley.com>
2023-01-06 23:12:06 -0800

    Update default formatters for javascript and yaml

--- programs/neovim/lua/mthadley/neoformat.lua
+++ programs/neovim/lua/mthadley/neoformat.lua
@@ -2,10 +2,11 @@
 -- https://prettier.io/docs/en/vim.html#neoformathttpsgithubcomsbdchdneoformat
 vim.g.neoformat_try_node_exe = true
 
+vim.g.neoformat_enabled_javascript = { 'prettier', 'standard' }
+vim.g.neoformat_enabled_json = {}
 vim.g.neoformat_enabled_ruby = {}
 vim.g.neoformat_enabled_sql = {}
-vim.g.neoformat_enabled_yaml = {}
-vim.g.neoformat_enabled_json = {}
+vim.g.neoformat_enabled_yaml = { 'prettier' }
 
 local augroup = vim.api.nvim_create_augroup('neoformat', { clear = true })
 
