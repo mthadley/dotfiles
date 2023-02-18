8e7985a441c769f8653790c0e65b0e3115b029cd
Michael Hadley <m@mthadley.com>
2023-02-17 16:50:13 -0800

    Get `vim-test` working with Rush monorepos

--- programs/neovim/lua/mthadley/vim_test.lua
+++ programs/neovim/lua/mthadley/vim_test.lua
@@ -1,11 +1,30 @@
 -- vim-test/vim-test
 vim.g['test#strategy'] = 'tslime'
+vim.g['test#javascript#jest#options'] = '--watch'
+
+if #vim.fs.find('rush.json', { upwards = true }) > 0 then
+  vim.g['test#javascript#jest#executable'] = 'rushx test'
+end
+
+local function with_project_root(command)
+  return function()
+    local markers = vim.fs.find({'package.json', '.git'}, {
+      path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
+      upward = true,
+    })
+
+    if #markers > 0 then
+      vim.g['test#project_root'] = vim.fs.dirname(markers[1])
+    end
+
+    vim.cmd(command)
+  end
+end
+
+vim.keymap.set('n', 'tt', with_project_root('TestFile'))
+vim.keymap.set('n', 'tn', with_project_root('TestNearest'))
+vim.keymap.set('n', 'ta', with_project_root('TestSuite'))
 
 -- jgdavey/tslime.vim
 vim.g.tslime_always_current_session = true
 vim.g.tslime_always_current_window = true
-
-vim.keymap.set('n', 'tt', ':TestFile<CR>')
-vim.keymap.set('n', 'tn', ':TestNearest<CR>')
-vim.keymap.set('n', 'ta', ':TestSuite<CR>')
-vim.keymap.set('n', 'tl', ':execute "Tmux bundle exec rubocop " . expand("%:p")<CR>')
