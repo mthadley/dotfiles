a38ffd706cd249109ba80cca586546f2d8c97a09
Michael Hadley <m@mthadley.com>
2023-07-02 10:49:36 -0700

    Use `LspAttach` autocommand rather than `on_attach` callback

--- programs/neovim/lua/mthadley/lspconfig.lua
+++ programs/neovim/lua/mthadley/lspconfig.lua
@@ -1,29 +1,30 @@
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local function on_attach(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  for keys, fn in pairs({
-     ['<C-]>'] = vim.lsp.buf.definition,
-     ['<C-k>'] = vim.lsp.buf.signature_help,
-     ['<leader>ca'] = vim.lsp.buf.code_action,
-     ['<leader>D'] = vim.lsp.buf.type_definition,
-     ['<leader>e'] = vim.diagnostic.open_float,
-     ['<leader>f'] = vim.lsp.buf.formatting,
-     ['<leader>rn'] = vim.lsp.buf.rename,
-     ['[d'] = vim.diagnostic.goto_prev,
-     [']d'] = vim.diagnostic.goto_next,
-     ['gD'] = vim.lsp.buf.declaration,
-     ['gd'] = vim.lsp.buf.definition,
-     ['gi'] = vim.lsp.buf.implementation,
-     ['gr'] = vim.lsp.buf.references,
-     ['K'] = vim.lsp.buf.hover,
-  }) do
-    vim.keymap.set('n', keys, fn, { buffer = bufnr })
-  end
-end
-
 local language_servers = { 'tsserver', 'zls' }
 for _, server in pairs(language_servers) do
   require('lspconfig')[server].setup { on_attach = on_attach }
 end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('mthadley:lspconfig', {}),
+  callback = function(event)
+    vim.api.nvim_buf_set_option(event.buf, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    for keys, fn in pairs({
+      ['<C-]>'] = vim.lsp.buf.definition,
+      ['<C-k>'] = vim.lsp.buf.signature_help,
+      ['<leader>ca'] = vim.lsp.buf.code_action,
+      ['<leader>D'] = vim.lsp.buf.type_definition,
+      ['<leader>e'] = vim.diagnostic.open_float,
+      ['<leader>f'] = vim.lsp.buf.formatting,
+      ['<leader>rn'] = vim.lsp.buf.rename,
+      ['[d'] = vim.diagnostic.goto_prev,
+      [']d'] = vim.diagnostic.goto_next,
+      ['gD'] = vim.lsp.buf.declaration,
+      ['gd'] = vim.lsp.buf.definition,
+      ['gi'] = vim.lsp.buf.implementation,
+      ['gr'] = vim.lsp.buf.references,
+      ['K'] = vim.lsp.buf.hover,
+    }) do
+      vim.keymap.set('n', keys, fn, { buffer = event.buf })
+    end
+  end
+})
