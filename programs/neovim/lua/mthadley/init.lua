59db343b79e06772d40eed4bc97013cca3ffb1e3
Michael Hadley <m@mthadley.com>
2023-08-19 10:36:20 -0700

    Dynamically load all user modules

--- programs/neovim/lua/mthadley/init.lua
+++ programs/neovim/lua/mthadley/init.lua
@@ -1,29 +1,16 @@
 vim.g.mapleader = ','
 
-local user_modules = {
-  'mthadley.options',
-  'mthadley.lspconfig',
-  'mthadley.gitsigns',
-  'mthadley.neoformat',
-  'mthadley.fugitive',
-  'mthadley.mappings',
-  'mthadley.cursorline',
-  'mthadley.filetype_quirks',
-  'mthadley.colors',
-  'mthadley.fzf',
-  'mthadley.vim_test',
-  'mthadley.vim_sort_motion',
-  'mthadley.netrw',
-  'mthadley.comment',
-  'mthadley.autopairs',
-  'mthadley.registers',
-}
+local user_config_dir =
+  vim.fs.dirname(vim.env.MYVIMRC) .. '/lua/mthadley'
 
-for _, module_path in ipairs(user_modules) do
-  local success, err = pcall(require, module_path)
+for file in vim.fs.dir(user_config_dir) do
+  if file ~= 'init.lua' then
+    local module_path = 'mthadley' .. '.' .. vim.fn.fnamemodify(file, ':r')
+    local success, err = pcall(require, module_path)
 
-  if not success then
-    print('Error while loading user module "' .. module_path .. '"')
-    print(err)
+    if not success then
+      print('Error while loading user module "' .. module_path .. '"')
+      print(err)
+    end
   end
 end
