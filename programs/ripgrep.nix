be76ab097f27fd0f8c6430291dbdec59e58ac35b
Michael Hadley <m@mthadley.com>
2023-12-17 15:13:33 -0800

    Use new `ripgrep` module
    The implementation results in essentially the same configuration, so may
    as well use it.

--- programs/ripgrep.nix
+++ programs/ripgrep.nix
@@ -1,22 +1,6 @@
-{ pkgs, ... }:
-
-let
-  ripgrepConfigPath = ".config/ripgrep/rgrc";
-in
 {
-  home = {
-    packages = [ pkgs.ripgrep ];
-
-    sessionVariables = {
-      RIPGREP_CONFIG_PATH = "$HOME/" + ripgrepConfigPath;
-    };
-
-    file.ripgrepConfig = {
-      text = ''
-        --hidden
-        --glob=!.git/*
-      '';
-      target = ripgrepConfigPath;
-    };
+  programs.ripgrep = {
+    enable = true;
+    arguments = [ "--hidden" "--glob=!.git/*" ];
   };
 }
