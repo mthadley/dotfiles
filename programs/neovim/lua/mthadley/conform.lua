4af094ce876feb0a156bae7fda61cc57ff55c7d2
Michael Hadley <m@mthadley.com>
2024-03-09 17:07:44 -0800

    Add more conform file types

--- programs/neovim/lua/mthadley/conform.lua
+++ programs/neovim/lua/mthadley/conform.lua
@@ -1,15 +1,18 @@
 require("conform").setup {
 	formatters_by_ft = {
+		css = { "prettier" },
 		fish = { "fish_indent" },
 		go = { "goimports", "gofmt" },
 		javascript = { "prettier" },
 		json = { "prettier" },
 		jsonc = { "prettier" },
 		lua = { "stylua" },
+		markdown = { "prettier" },
 		mdx = { "prettier" },
 		nix = { "nixpkgs_fmt" },
 		typescript = { "prettier" },
 		typescriptreact = { "prettier" },
+		yaml = { "prettier" },
 		zig = { "zigfmt" },
 	},
 	format_on_save = {
