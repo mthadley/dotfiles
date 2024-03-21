53f93d9d6542748da3f6062d6d4eec295346b4e0
Michael Hadley <m@mthadley.com>
2024-03-21 12:31:37 -0700

    Fix file type quirks and detection

--- programs/neovim/lua/mthadley/filetype_quirks.lua
+++ programs/neovim/lua/mthadley/filetype_quirks.lua
@@ -8,13 +8,23 @@ vim.g.vim_markdown_fenced_languages = { "js=javascript", "ini=dosini" }
 
 local group = vim.api.nvim_create_augroup("mthadley:filetype-quirks", { clear = true })
 
-vim.api.nvim_create_autocmd("BufRead", {
-	callback = function()
-		vim.opt.ft = "jsonc"
-	end,
-	desc = "JSON files that can have comments",
-	pattern = { "rush.json" },
-	group = group,
+vim.filetype.add({
+	extension = {
+		-- Treat MDX files as markdown until there's a first-class Treesitter
+		-- parser.
+		mdx = "markdown",
+	},
+	filename = {
+		-- JSON configuration files that can contain comments
+		["rush.json"] = "jsonc",
+
+		-- Treat Prettier ignore files as a .gitignore
+		[".prettierignore"] = "gitignore",
+	},
+	pattern = {
+		-- Recognize go templates
+		["*.go.tmpl"] = "gohtmltmpl",
+	},
 })
 
 vim.api.nvim_create_autocmd("FileType", {
@@ -24,23 +34,5 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 	desc = "Wrap lines when writing Markdown",
 	pattern = "markdown",
-	group = group,
-})
-
-vim.api.nvim_create_autocmd("BufRead", {
-	callback = function()
-		vim.opt.ft = "gohtmltmpl"
-	end,
-	desc = "Recognize go templates",
-	pattern = "*.go.tmpl",
-	group = group,
-})
-
-vim.api.nvim_create_autocmd("BufRead", {
-	callback = function()
-		vim.opt.ft = "gitignore"
-	end,
-	desc = "Treat Prettier ignore files as a .gitignore",
-	pattern = ".prettierignore",
 	group = group,
 })
