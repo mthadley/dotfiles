9f3fa28a2d84dcad9188d0468b2ebb07a695ad11
Michael Hadley <m@mthadley.com>
2024-04-01 13:13:48 -0700

    New git aliases

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -16,6 +16,8 @@
     shellAliases = {
       # git
       gco = "git checkout";
+      gsw = "git switch";
+      gre = "git restore";
       gd = "git diff";
       gplog = "git log --graph --pretty=format:\"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset\" --abbrev-commit --date=relative";
       gst = "git status";
