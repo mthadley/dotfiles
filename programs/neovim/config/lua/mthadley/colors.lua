7a5b8658327f8255d360dbc373151cc06bda91e0
Michael Hadley <m@mthadley.com>
2024-04-06 20:56:36 -0700

    Switch to `nvim-base16`

--- programs/neovim/config/lua/mthadley/colors.lua
+++ programs/neovim/config/lua/mthadley/colors.lua
@@ -1,29 +1,35 @@
--- chriskempson/base16-vim
-vim.g.base16colorspace = 256
-
-vim.opt.termguicolors = true
 vim.cmd "colorscheme base16-ocean"
 
-local color_links = {
-	FloatBorder = "Comment",
-	FzfLuaBorder = "FloatBorder",
+local function hi(group, options)
+	local ns_id = 0
+	local current_options = vim.api.nvim_get_hl(ns_id, { name = group })
 
-	-- LSP-related Colors
-	DiagnosticError = "ErrorMsg",
-	DiagnosticWarn = "WarningMsg",
-	DiagnosticInfo = "MoreMsg",
-}
+	-- Inherit existing options rather than clearing them out
+	if not current_options.link then
+		options = vim.tbl_extend("keep", options, current_options)
+	end
 
-for source, target in pairs(color_links) do
-	vim.api.nvim_set_hl(0, source, { link = target })
+	vim.api.nvim_set_hl(ns_id, group, options)
 end
 
-local diagnostic_sign_colors = {
-	DiagnosticSignError = vim.g.base16_gui08,
-	DiagnosticSignHint = vim.g.base16_gui05,
-	DiagnosticSignWarning = vim.g.base16_gui09,
-}
+hi("FloatBorder", { link = "Comment" })
+hi("FzfLuaBorder", { link = "FloatBorder" })
 
-for group, fg in pairs(diagnostic_sign_colors) do
-	vim.api.nvim_set_hl(0, group, { fg = "#" .. fg, bg = "#" .. vim.g.base16_gui01 })
-end
+local colors = require("base16-colorscheme").colors
+
+local gray_bg = colors.base01
+local gray_fg = colors.base03
+local white_fg = colors.base05
+local red_fg = colors.base08
+local yellow_fg = colors.base09
+
+-- Line number and gutter customizations
+hi("CursorLineNr", { fg = white_fg })
+hi("LineNr", { fg = gray_fg, bg = gray_bg })
+hi("SignColumn", { bg = gray_bg })
+hi("DiagnosticSignError", { fg = red_fg, bg = gray_bg })
+hi("DiagnosticSignHint", { fg = white_fg, bg = gray_bg })
+hi("DiagnosticSignWarning", { fg = yellow_fg, bg = gray_bg })
+hi("GitGutterChange", { bg = gray_bg })
+hi("GitGutterAdd", { bg = gray_bg })
+hi("GitGutterDelete", { bg = gray_bg })
