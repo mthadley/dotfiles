5a8fa5b36062600b5530210248e470d2f1fc4ae9
Michael Hadley <m@mthadley.com>
2024-04-19 14:34:26 -0700

    Don't automatically require directories

--- programs/neovim/config/lua/mthadley/init.lua
+++ programs/neovim/config/lua/mthadley/init.lua
@@ -2,8 +2,15 @@ vim.g.mapleader = ","
 
 local user_config_dir = vim.fs.dirname(vim.env.MYVIMRC) .. "/lua/mthadley"
 
-for file in vim.fs.dir(user_config_dir) do
-	if file ~= "init.lua" then
+for file, type in vim.fs.dir(user_config_dir) do
+	-- `vim.fs.dir` currently does not resolve links, so fall
+	-- back to `uv.fs_stat` which does.
+	if type == "link" then
+		local file_path = user_config_dir .. "/" .. file
+		type = vim.loop.fs_stat(file_path).type
+	end
+
+	if type == "file" and file ~= "init.lua" then
 		local module_path = "mthadley" .. "." .. vim.fn.fnamemodify(file, ":r")
 		local success, err = pcall(require, module_path)
 
