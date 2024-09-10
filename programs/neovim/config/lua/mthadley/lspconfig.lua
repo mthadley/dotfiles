e1ab1c2d0674c9d782d6a19700e01cf201ca05ec
Michael Hadley <m@mthadley.com>
2024-09-10 09:25:05 -0700

    Fix double-dot issue with LSP and `tsserver`

--- programs/neovim/config/lua/mthadley/lspconfig.lua
+++ programs/neovim/config/lua/mthadley/lspconfig.lua
@@ -9,6 +9,12 @@ lspconfig.tsserver.setup {
 	-- Override default and only start if a root directory with a
 	-- `tsconfig.json` is found.
 	single_file_support = false,
+
+	init_options = {
+		-- Fixes double dot insertion when auto-completing with the omnifunc.
+		-- https://github.com/typescript-language-server/typescript-language-server/issues/711
+		completionDisableFilterText = true,
+	},
 }
 
 lspconfig.denols.setup {
