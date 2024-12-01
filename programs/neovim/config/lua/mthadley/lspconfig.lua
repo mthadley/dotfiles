9f2e01f444493ac049276c334ed5f0b6470413d6
Michael Hadley <m@mthadley.com>
2024-11-30 17:17:10 -0800

    These are now default mappings
    From `0.10` changelog:
    
    > * "]d" and "[d" in Normal mode map to vim.diagnostic.goto_next()
      and vim.diagnostic.goto_prev(), respectively. ]d-default [d-default
    > * <C-W>d (and <C-W><C-D>) map to vim.diagnostic.open_float() CTRL-W_d-default
    > * vim.lsp.start() sets the following default keymaps (assuming server support):
    >   * K in Normal mode maps to vim.lsp.buf.hover(), unless
    >    'keywordprg' was customized before calling vim.lsp.start().

--- programs/neovim/config/lua/mthadley/lspconfig.lua
+++ programs/neovim/config/lua/mthadley/lspconfig.lua
@@ -56,13 +56,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 			["<leader>e"] = vim.diagnostic.open_float,
 			["<leader>f"] = vim.lsp.buf.formatting,
 			["<leader>rn"] = vim.lsp.buf.rename,
-			["[d"] = vim.diagnostic.goto_prev,
-			["]d"] = vim.diagnostic.goto_next,
 			["gD"] = vim.lsp.buf.declaration,
 			["gd"] = vim.lsp.buf.definition,
 			["gi"] = vim.lsp.buf.implementation,
 			["gr"] = vim.lsp.buf.references,
-			["K"] = vim.lsp.buf.hover,
 		}) do
 			vim.keymap.set("n", keys, fn, { buffer = event.buf })
 		end
