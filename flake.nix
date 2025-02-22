d691e5b2dfe558318f3d9c789c2a31fa680a02b6
Michael Hadley <m@mthadley.com>
2025-02-21 16:16:00 -0800

    Use `eachDefaultSystemMap` rather than hardcoding system

--- flake.nix
+++ flake.nix
@@ -16,50 +16,50 @@
   };
 
   outputs = { nixpkgs, home-manager, flake-utils, zimilar-zort, ... }:
-    let
-      systems = flake-utils.lib.system;
-
-      mkHomeConfig = { system, username, homeDirectory, name }: {
-        ${name} = home-manager.lib.homeManagerConfiguration
-          {
-            pkgs = nixpkgs.legacyPackages.${system};
-            modules = [
-              ./home.nix
-              {
-                home = {
-                  inherit username homeDirectory;
-
-                  sessionVariables = {
-                    HOME_MANAGER_CONFIG = name;
-                  };
-
-                  # Update the state version as needed.
-                  # See the changelog here:
-                  # https://nix-community.github.io/home-manager/release-notes.html#sec-release-21.05
-                  stateVersion = "23.11";
-                };
-
-                nixpkgs.overlays = [ zimilar-zort.overlays.${system}.default ];
-              }
-            ];
-          };
-      };
-    in
     {
-      homeConfigurations =
-        mkHomeConfig
-          rec {
-            homeDirectory = "/Users/${username}";
-            name = "mthadley-workos";
-            system = systems.aarch64-darwin;
-            username = "michael.hadley";
-          } //
+      packages = flake-utils.lib.eachDefaultSystemMap (system:
+        let
+          mkHomeConfig = { username, homeDirectory, name }: {
+            ${name} = home-manager.lib.homeManagerConfiguration
+              {
+                pkgs = nixpkgs.legacyPackages.${system};
+                modules = [
+                  ./home.nix
+                  {
+                    home = {
+                      inherit username homeDirectory;
 
-        mkHomeConfig rec {
-          homeDirectory = "/Users/${username}";
-          name = "mthadley-home";
-          system = systems.aarch64-darwin;
-          username = "mthadley";
-        };
+                      sessionVariables = {
+                        HOME_MANAGER_CONFIG = name;
+                      };
+
+                      # Update the state version as needed.
+                      # See the changelog here:
+                      # https://nix-community.github.io/home-manager/release-notes.html#sec-release-21.05
+                      stateVersion = "23.11";
+                    };
+
+                    nixpkgs.overlays = [ zimilar-zort.overlays.${system}.default ];
+                  }
+                ];
+              };
+          };
+        in
+        {
+          homeConfigurations =
+            mkHomeConfig
+              rec {
+                homeDirectory = "/Users/${username}";
+                name = "mthadley-workos";
+                username = "michael.hadley";
+              } //
+
+            mkHomeConfig rec {
+              homeDirectory = "/Users/${username}";
+              name = "mthadley-home";
+              username = "mthadley";
+            };
+        }
+      );
     };
 }
