26719cce58db39d69c5692415287af05dd70dd27
Michael Hadley <m@mthadley.com>
2025-03-28 17:37:44 -0700

    Add Rush build shortcut

--- programs/neovim/config/lua/mthadley/mappings.lua
+++ programs/neovim/config/lua/mthadley/mappings.lua
@@ -26,3 +26,38 @@ keymap.set("n", "<leader>cw", function()
 
 	vim.cmd.copen()
 end, { silent = true })
+
+-- Run rush build for the package containing the current buffer
+keymap.set("n", "<leader>b", function()
+	local rush_root = vim.fs.find({ "rush.json" }, {
+		path = vim.api.nvim_buf_get_name(0),
+		upward = true,
+	})[1]
+	if not rush_root then
+		vim.print("No rush.json found. Ostensibly not a Rush project.")
+		return
+	end
+	local rush_root_dir = vim.fn.fnamemodify(rush_root, ":p:h")
+
+	-- Get path of the current buffer
+	local package_json = vim.fs.find({ "package.json" }, {
+		path = vim.api.nvim_buf_get_name(0),
+		upward = true,
+		stop = rush_root,
+	})[1]
+
+	local target_options = ""
+	if package_json then
+		local package_dir = vim.fs.dirname(package_json)
+
+		if package_dir == rush_root_dir then
+			vim.print("No package.json found. Building the entire project.")
+		else
+			local package_name = vim.fs.basename(package_dir)
+			target_options = " --to=" .. package_name
+		end
+	end
+
+	-- Depends on `tslime` for this `Tmux` command
+	vim.cmd.Tmux("rush build" .. target_options)
+end, { silent = true })
