8c9976645f0ab4bf3f7b3da1b0852a88cb3837d7
Michael Hadley <m@mthadley.com>
2025-03-29 19:49:54 -0700

    Use method syntax

--- programs/neovim/config/after/ftplugin/lua.lua
+++ programs/neovim/config/after/ftplugin/lua.lua
@@ -2,6 +2,6 @@ local buf_path = vim.api.nvim_buf_get_name(0)
 
 -- Fuzzily assume we are editing some `.lua` files related
 -- to Neovim and enable the builtin completion.
-if string.match(buf_path, "neovim") then
+if buf_path:match("neovim") then
 	vim.opt_local.omnifunc = "v:lua.vim.lua_omnifunc"
 end
