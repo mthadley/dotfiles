0691b17b432e476b1301912b227a8a01c39c6c86
Michael Hadley <m@mthadley.com>
2025-11-19 19:38:50 -0800

    Simplify

--- programs/nix.nix
+++ programs/nix.nix
@@ -1,8 +1,5 @@
 {
-  home.file.nixConfig = {
-    text = ''
-      experimental-features = nix-command flakes
-    '';
-    target = ".config/nix/nix.conf";
-  };
+  home.file.".config/nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+  '';
 }
