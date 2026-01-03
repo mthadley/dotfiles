0f07704c886311ae627822cf014274fde2eddcf5
Michael Hadley <m@mthadley.com>
2026-01-02 23:34:51 -0800

    Some fzf color tweaks

--- programs/fzf.nix
+++ programs/fzf.nix
@@ -10,7 +10,12 @@ in
     defaultCommand = defaultFilesCommand;
     fileWidgetCommand = defaultFilesCommand;
 
-    defaultOptions = [ "--color=16" ];
+    # Some suble tweaks to align with my `base16-ocean` preference. Actual
+    # values were pulled from here:
+    #
+    #   https://github.com/RRethy/base16-nvim/blob/e6b3b10263288096653c3cfbcb144790874ef1a7/lua/colors/ocean.lua
+    #
+    defaultOptions = [ "--color=16,pointer:#ebcb8b,bg+:#343d46" ];
 
     enableFishIntegration = true;
   };
