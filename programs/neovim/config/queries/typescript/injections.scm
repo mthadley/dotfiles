06becfade1b5f94335c7e5acae1ad1be3155082c
Michael Hadley <m@mthadley.com>
2026-01-29 17:27:37 -0800

    Cover more cases

--- programs/neovim/config/queries/typescript/injections.scm
+++ programs/neovim/config/queries/typescript/injections.scm
@@ -1,11 +1,15 @@
 ;; extends
 
-;; SQL tagged template literals: sql.*(...)`SELECT ...`
-;; Matches: sql.type(...)`...`, sql.unsafe(...)`...`, sql.fragment(...)`...`, etc.
+;; SQL tagged template literals
+;; Matches: sql.unsafe`...`, sql.type(...)`...`, etc.
 (call_expression
-  function: (call_expression
-    function: (member_expression
-      object: (identifier) @_sql_id))
+  function: [
+    (member_expression
+      object: (identifier) @_sql_id)
+    (call_expression
+      function: (member_expression
+        object: (identifier) @_sql_id))
+  ]
   arguments: (template_string
     (string_fragment) @injection.content)
   (#eq? @_sql_id "sql")
