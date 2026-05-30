be2ab4686d9d906c1b67f2b0e7a706d953093f01
Michael Hadley <m@mthadley.com>
2026-05-30 14:35:11 -0700

    Fix broken treesitter configuration
    Looks like the totally removed `.setup` and now you need to assemble all
    of these pieces yourself. Annoying.

--- programs/neovim/config/lua/mthadley/treesitter.lua
+++ programs/neovim/config/lua/mthadley/treesitter.lua
@@ -1,56 +1,45 @@
--- The default intall path is somewhere under `/nix` which
--- is not writable, we need to install parsers somewhere else.
-local parser_install_dir = vim.fn.expand("~/.local/share/nvim/treesitter/parsers")
-
--- The custom install path needs to also be added to the "rtp". Uses
--- `prepend` instead of `append` so that we don't use the bundled
--- parsers which seem to be out of date and throw errors.
-vim.opt.runtimepath:prepend(parser_install_dir)
-
 -- TODO: Why is this needed? I get compilation errors when using the plugin
 -- default of `clang`.
 require("nvim-treesitter.install").compilers = { "gcc" }
 
-require("nvim-treesitter").setup {
-	parser_install_dir = parser_install_dir,
+require("nvim-treesitter").install {
+	-- These are reccomended to always be installed, since they
+	-- relate to general usage of Neovim.
+	"c",
+	"lua",
+	"query",
+	"vim",
+	"vimdoc",
 
-	ensure_installed = {
-		-- These are reccomended to always be installed, since they
-		-- relate to general usage of Neovim.
-		"c",
-		"lua",
-		"query",
-		"vim",
-		"vimdoc",
-
-		-- Languages I often work with.
-		"bash",
-		"embedded_template",
-		"fish",
-		"html",
-		"javascript",
-		"json",
-		"markdown",
-		"markdown_inline",
-		"nix",
-		"ruby",
-		"typescript",
-		"yaml",
-	},
-
-	auto_install = true,
-
-	highlight = {
-		enable = true,
-	},
-
-	indent = {
-		enable = true,
-		disable = {
-			-- The indentation is weird on Ruby right now, so falling
-			-- back to the non-treesitter default, but maybe can be removed
-			-- in the future.
-			"ruby",
-		},
-	},
+	-- Languages I often work with.
+	"bash",
+	"embedded_template",
+	"fish",
+	"html",
+	"javascript",
+	"json",
+	"markdown",
+	"markdown_inline",
+	"nix",
+	"ruby",
+	"typescript",
+	"yaml",
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("mthadley:treesitter", { clear = true }),
+	callback = function(args)
+		local lang = vim.treesitter.language.get_lang(args.match)
+		if not lang or not vim.treesitter.language.add(lang) then
+			return
+		end
+
+		if vim.treesitter.query.get(lang, "highlights") then
+			vim.treesitter.start(args.buf)
+		end
+
+		if vim.treesitter.query.get(lang, "indents") then
+			vim.opt_local.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
+		end
+	end,
+})
